800bfaf08de818dfa6dd7c892321b6a609176d2c
kokotata421 <kokotata421@gmail.com>
2019-06-10 19:25:20 +0900

    initial Commit

---
+++ iOSArchitectureKKSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/06/10.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
