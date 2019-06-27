ead1766467e5b1f4dd6a78e69aa9f04bd38f122e
kokotata421 <kokotata421@gmail.com>
2019-06-28 06:24:37 +0900

    [change] MainTabCoordinatorの位置を変更
    MainTabCoordinatorのファイルがプロジェクト外に置かれていたので、一度削除して再度生成

--- iOSArchitectureKKSample/MainTabCoordinator.swift
+++ iOSArchitectureKKSample/MainTabCoordinator.swift
@@ -1,14 +1,14 @@
 //
 //  MainTabCoordinator.swift
-//  
+//  iOSArchitectureKKSample
 //
-//  Created by Kota Kawanishi on 2019/06/27.
+//  Created by Kota Kawanishi on 2019/06/28.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
 //
 
 import Foundation
 
 final class MainTabCoordinator: Coordinator {
-    
     func start() {
         return
     }
