b7171a96d13eb5264cbbbe28730a3b32276413b2
kokotata421 <kokotata421@gmail.com>
2019-06-28 18:29:01 +0900

    [add] MainTabCoordinatorの実装
    MainTabControllerと各タブに対応するCoordinatorをMainTabControllerに宣言

--- iOSArchitectureKKSample/MainTabCoordinator.swift
+++ iOSArchitectureKKSample/MainTabCoordinator.swift
@@ -6,9 +6,19 @@
 //  Copyright © 2019 Kota Kawanishi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 final class MainTabCoordinator: Coordinator {
+    let mainTabBarController: UITabBarController = .init()
+    let categoryCoordinator: ColourCategoryCoordinator = .init()
+    let searchCoordinator: ColourSearchCoordinator = .init()
+    let favoritesCoordinator: ColourFavoritesCoordinator = .init()
+    let settingCoordinator: ColourSettingCoordinator = .init()
+    
+    init() {
+        
+    }
+    
     func start() {
         return
     }
