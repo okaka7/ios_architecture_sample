7ebd7d66fd7ead23bd37ec6e215982d757008c15
kokotata421 <kokotata421@gmail.com>
2019-07-13 00:21:59 +0900

    [add]クラスの名前を取得するエクステンションを追加

---
+++ iOSArchitectureKKSample/NSObject+ClassName.swift
@@ -0,0 +1,30 @@
+//
+//  NSObject+ClassName.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/13.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+
+public protocol ClassNameProtocol {
+    static var className: String { get }
+    var className: String { get }
+}
+
+public extension ClassNameProtocol {
+    static var className: String {
+        return String(describing: self)
+    }
+    
+    var className: String {
+        return type(of: self).className
+    }
+}
+
+extension NSObject: ClassNameProtocol {
+}
