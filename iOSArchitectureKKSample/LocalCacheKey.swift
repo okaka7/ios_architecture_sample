f64a61076c6de7382bd5651973e95a97b95b13a1
kokotata421 <kokotata421@gmail.com>
2019-07-13 00:41:55 +0900

    [add] UserDefaultをラップしたLocalCache追加

---
+++ iOSArchitectureKKSample/LocalCacheKey.swift
@@ -0,0 +1,21 @@
+//
+//  LocalCacheKey.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/13.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol LocalCacheKeys {}
+
+struct LocalCacheKey<Value: LocalCacheValue>: LocalCacheKeys {
+    let rawValue: String
+    let defaultValue: Value?
+    
+    fileprivate init(_ rawValue: String, defaultValue: Value?) {
+        self.rawValue = rawValue
+        self.defaultValue = defaultValue
+    }
+}
