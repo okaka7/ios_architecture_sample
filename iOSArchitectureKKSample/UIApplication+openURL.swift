2948dcd82c150296101f6ec281c4f19bf20e476f
kokotata421 <kokotata421@gmail.com>
2019-07-13 00:20:50 +0900

    [add] UIApplicationでurlを開くときのエクステンションメソッドを追加

---
+++ iOSArchitectureKKSample/UIApplication+openURL.swift
@@ -0,0 +1,23 @@
+//
+//  UIApplication+openURL.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/12.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplication {
+    @discardableResult
+    func openURLIfPossible(_ url: URL,
+                           options: [UIApplication.OpenExternalURLOptionsKey : Any],
+                           completionHandler: ((Bool) -> Void)? = nil) -> Bool {
+        
+        if self.canOpenURL(url) {
+            self.open(url, options: options, completionHandler: completionHandler)
+            return true
+        }
+        return false
+    }
+}
