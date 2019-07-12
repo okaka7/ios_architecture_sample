2f0dd5d47c26dd3e1e2bfe90fb9e953435072cb1
kokotata421 <kokotata421@gmail.com>
2019-07-13 00:48:05 +0900

    [add] dribbbleOauthで第三者に改ざんがされていないかstringで確認するため、そのためのDribbbleState構造体を追加

---
+++ iOSArchitectureKKSample/DribbbleState.swift
@@ -0,0 +1,27 @@
+//
+//  DribbbleState.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/13.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+struct DribbbleState: ExpressibleByStringLiteral, Equatable {
+    typealias StringLiteralType = String
+    
+    let state: String
+    
+    init(stringLiteral value: DribbbleState.StringLiteralType) {
+        self.state = value
+    }
+    
+    static func ==(lhs: DribbbleState, rhs: DribbbleState) -> Bool {
+        return lhs.state == rhs.state
+    }
+    
+    static func ==(lhs: DribbbleState, rhs: String) -> Bool {
+        return lhs.state == rhs
+    }
+}
