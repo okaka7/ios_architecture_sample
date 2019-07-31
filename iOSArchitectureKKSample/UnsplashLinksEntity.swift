c9f6304ab3698a2dac5cf32fe3e0a35ead7b68a6
kokotata421 <kokotata421@gmail.com>
2019-07-31 18:02:29 +0900

    [change] UnsplashUserEntityに入っていたLinksに関するエクステンションをUnsplashLinksEntity.swiftに移行

--- iOSArchitectureKKSample/UnsplashLinksEntity.swift
+++ iOSArchitectureKKSample/UnsplashLinksEntity.swift
@@ -16,3 +16,30 @@ struct Links: Codable {
         case html, photos, likes, portfolio
     }
 }
+
+extension Links {
+    init?(data: Data) {
+        guard let me = try? JSONDecoder().decode(Links.self, from: data) else { return nil }
+        self = me
+    }
+    
+    init?(_ json: String, using encoding: String.Encoding = .utf8) {
+        guard let data = json.data(using: encoding) else { return nil }
+        self.init(data: data)
+    }
+    
+    init?(fromURL url: String) {
+        guard let url = URL(string: url) else { return nil }
+        guard let data = try? Data(contentsOf: url) else { return nil }
+        self.init(data: data)
+    }
+    
+    var jsonData: Data? {
+        return try? JSONEncoder().encode(self)
+    }
+    
+    var json: String? {
+        guard let data = self.jsonData else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+}
