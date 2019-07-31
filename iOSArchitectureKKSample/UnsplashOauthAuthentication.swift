fbcc6938f5e739e26899a6036233bcb0ffc81a81
kokotata421 <kokotata421@gmail.com>
2019-07-31 18:20:36 +0900

    [add] Unsplash(写真)APIのOauth認可処理のモデルクラス(UnsplashOauthAuthenticationとUnsplashOauthToken)の追加

---
+++ iOSArchitectureKKSample/UnsplashOauthAuthentication.swift
@@ -0,0 +1,14 @@
+//
+//  UnsplashOauthAuthentication.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/31.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+
+struct UnsplashOauthAuthentication {
+    
+}
---
+++ iOSArchitectureKKSample/UnsplashOauthToken.swift
@@ -0,0 +1,13 @@
+//
+//  UnsplashOauthToken.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/07/31.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+struct UnsplashOauthToken {
+  
+}
