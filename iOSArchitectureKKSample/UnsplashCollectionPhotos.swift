d9ad6dd18305623fd83b1a979f2094cc95120e3b
kokotata421 <kokotata421@gmail.com>
2019-08-10 22:48:35 +0900

    [add] Unsplashのコレクションの写真を取得するエンドポイントUnsplashCollectionPhotosTargetを追加

---
+++ iOSArchitectureKKSample/UnsplashCollectionPhotos.swift
@@ -0,0 +1,34 @@
+//
+//  UnsplashCollectionPhotos.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/10.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+struct UnsplashCollectionPhotosTarget: UnsplashAPITargetType {
+    
+    typealias Response = [UnsplashPhotoEntity]
+    
+    var path: String {
+        return "/collections/\(id)/photos"
+    }
+    let method: Moya.Method = .get
+    let id: String
+    var task: Task {
+        return .requestPlain
+    }
+    
+    init(id: String) {
+        self.id = id
+    }
+}
+
+extension UnsplashCollectionPhotosTarget: AccessTokenAuthorizable {
+    var authorizationType: AuthorizationType {
+        return .bearer
+    }
+}
