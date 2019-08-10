723a29c5540adc7a93f74df908f72cbdde004b90
kokotata421 <kokotata421@gmail.com>
2019-08-10 20:16:52 +0900

    [add] unsplashAPIでユーザーのポートフォリオを取得するエンドポイントを追加

--- iOSArchitectureKKSample/UnsplashPortfolio.swift
+++ iOSArchitectureKKSample/UnsplashPortfolio.swift
@@ -1,43 +1,40 @@
 //
-//  UnsplashLinksEntity.swift
+//  UnsplashPortfolio.swift
 //  iOSArchitectureKKSample
 //
-//  Created by Kota Kawanishi on 2019/07/31.
+//  Created by Kota Kawanishi on 2019/08/10.
 //  Copyright © 2019 Kota Kawanishi. All rights reserved.
 //
 
 import Foundation
 
-struct Links: Codable {
-    let selfLink, html, photos, likes, portfolio: String
-    
-    enum CodingKeys: String, CodingKey {
-        case selfLink = "self"
-        case html, photos, likes, portfolio
-    }
+struct UnsplashPortfolio: Codable {
+    let url: String
 }
 
-extension Links {
+// MARK: Convenience initializers
+
+extension UnsplashPortfolio {
     init?(data: Data) {
-        guard let me = try? JSONDecoder().decode(Links.self, from: data) else { return nil }
+        guard let me = try? JSONDecoder().decode(UnsplashPortfolio.self, from: data) else { return nil }
         self = me
     }
-    
+
     init?(_ json: String, using encoding: String.Encoding = .utf8) {
         guard let data = json.data(using: encoding) else { return nil }
         self.init(data: data)
     }
-    
+
     init?(fromURL url: String) {
         guard let url = URL(string: url) else { return nil }
         guard let data = try? Data(contentsOf: url) else { return nil }
         self.init(data: data)
     }
-    
+
     var jsonData: Data? {
         return try? JSONEncoder().encode(self)
     }
-    
+
     var json: String? {
         guard let data = self.jsonData else { return nil }
         return String(data: data, encoding: .utf8)
---
+++ iOSArchitectureKKSample/UserPortfolio.swift
@@ -0,0 +1,34 @@
+//
+//  UserPortfolio.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/10.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+
+import Foundation
+import Moya
+
+struct UnsplashUserPortfolio: UnsplashAPITargetType {
+    typealias Response = UnsplashPortfolio
+    
+    var path: String {
+        return "/users/\(userName)/portfolio"
+    }
+    let method: Moya.Method = .get
+    let userName: String
+    var task: Task {
+        return .requestPlain
+    }
+    
+    init(userName: String) {
+        self.userName = userName
+    }
+}
+
+extension UnsplashUserPortfolio: AccessTokenAuthorizable {
+    var authorizationType: AuthorizationType {
+        return .bearer
+    }
+}
