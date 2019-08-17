89f8c301beaf13b3beeeb747792cc49171cc58ab
kokotata421 <kokotata421@gmail.com>
2019-08-18 00:49:51 +0900

    [change/ add] APIトークン用のリポジトリを作成

---
+++ iOSArchitectureKKSample/API Token/TokenRepository.swift
@@ -0,0 +1,14 @@
+//
+//  TokenRepository.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/18.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol APITokenRepository {
+    func saveToken(token: String)
+    func fetchToken() -> String
+}
