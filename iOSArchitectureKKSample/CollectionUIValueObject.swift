6b8361e264b6cad58dd3cd46c8efb0e73b7c0511
kokotata421 <kokotata421@gmail.com>
2019-08-19 05:27:10 +0900

    [add] collectionのUI用Objectを追加

---
+++ iOSArchitectureKKSample/CollectionUIValueObject.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionUIValueObject.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/19.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+struct CollectionUIValueObject {
+    let id: Int
+    let title: String
+    let description: String?
+    //let publishedAt, updatedAt: String
+    let curated, featured: Bool
+    let totalPhotos: Int
+    let purplePrivate: Bool
+    let shareKey: String
+    //let tags: [Tag]
+    //let links: UnsplashCollectionVOLinks
+    let user: UnsplashUserValueObject
+    let coverPhoto: CoverPhoto
+    let previewPhotos: [Photo]
+}
