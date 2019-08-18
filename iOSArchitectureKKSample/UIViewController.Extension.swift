8800d9adfd27c279226b6a2382d1c2edf646ab70
kokotata421 <kokotata421@gmail.com>
2019-08-19 02:55:25 +0900

    [add] Extensionファイルを利用してViewController機能を拡張するUIViewContrller.Extensionファイルを追加

---
+++ iOSArchitectureKKSample/UIViewController.Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewController.Extension.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/19.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController: ExtensionCompatible {}
+
+extension Extension where Base: UIViewController {
+    
+}
