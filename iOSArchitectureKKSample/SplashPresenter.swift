32d59d2eed0a95fb7a62fe4ccf0986b5ce032572
kokotata421 <kokotata421@gmail.com>
2019-08-18 23:32:14 +0900

    [change] splash画面のPresenterを追加

---
+++ iOSArchitectureKKSample/SplashPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  SplashPresenter.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/18.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
