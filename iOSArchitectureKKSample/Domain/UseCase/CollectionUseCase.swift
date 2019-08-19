45074f971726498c3a7eacac19eac91b8b9228c7
kokotata421 <kokotata421@gmail.com>
2019-08-19 16:30:50 +0900

    [add] collectionUsecaseを追加

---
+++ iOSArchitectureKKSample/Domain/UseCase/CollectionUseCase.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionUseCase.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/19.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol CollectionUseCaseInputPort {
+    func openCollection(id: String)
+    func addPhoto(id: String, into collectionID: String)
+    func removePhoto(id: String, from collectionID: String)
+    
+}
+
+protocol CollectionUseCaseOutputPort {
+}
