bd670fd72027c9c63df6703efd56708c245149a5
kokotata421 <kokotata421@gmail.com>
2019-08-24 04:10:33 +0900

    [add 🎉] DataStoreをRxでラップするファイル追加
    DataStore+rx.swiftを追加して、LocalCache,KeyChainCacheをReactiveCompatibleに準拠させる

---
+++ iOSArchitectureKKSample/DataStore+Rx.swift
@@ -0,0 +1,14 @@
+//
+//  DataStore+rx.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/24.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+extension LocalCache: ReactiveCompatible {}
+extension KeyChainCache: ReactiveCompatible {}
+
