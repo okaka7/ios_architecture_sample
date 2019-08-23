507cd3de8c5dc17b1be1b63d98585ba0fe2ebd60
kokotata421 <kokotata421@gmail.com>
2019-08-24 02:22:17 +0900

    [add 🎉] SearchViewの入力、出力を処理するSearchViewAdapterを追加

---
+++ iOSArchitectureKKSample/SearchViewAdapter.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewAdapter.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/24.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchViewController {
+    
+}
+
+protocol SearchViewPresenter {
+    
+}
+
+struct SearchViewAdapter: SearchViewController, SearchViewPresenter {
+    
+}
