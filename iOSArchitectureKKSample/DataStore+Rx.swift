8582307e3d7e1d10b266d3cb03a1c8c29c6b8ad2
kokotata421 <kokotata421@gmail.com>
2019-08-24 15:13:31 +0900

    [add 🎉] DataStoreをRxでラップ
    Cacheableのsave, fetch,deleteメソッドをrx化

--- iOSArchitectureKKSample/DataStore+Rx.swift
+++ iOSArchitectureKKSample/DataStore+Rx.swift
@@ -12,3 +12,29 @@ import RxSwift
 extension LocalCache: ReactiveCompatible {}
 extension KeyChainCache: ReactiveCompatible {}
 
+public extension Reactive where Base: Cacheable {
+    func save<T: CacheValue>(key: CacheKey<T>, value: T) -> Completable {
+        return Completable.create { (observer: @escaping PrimitiveSequenceType.CompletableObserver) -> Disposable in
+                self.base.save(key, value: value)
+                observer(CompletableEvent.completed)
+            return Disposables.create()
+        }
+    }
+    
+    func fetch<T: CacheValue>(key: CacheKey<T>) -> Maybe<T?> {
+        return Observable<T?>.create { observer in
+            let element = self.base.fetch(key)
+            observer.on(.next(element))
+            observer.on(.completed)
+            return Disposables.create()
+        }.asMaybe()
+    }
+  
+    func delete<T: CacheValue>(key: CacheKey<T>) -> Completable {
+        return Completable.create { (observer: @escaping PrimitiveSequenceType.CompletableObserver) -> Disposable in
+            self.base.delete(key)
+            observer(CompletableEvent.completed)
+            return Disposables.create()
+        }
+    }
+}
