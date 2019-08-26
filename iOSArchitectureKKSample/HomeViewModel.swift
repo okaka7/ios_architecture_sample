60f8756f13fa3dcfa318b6c2629dabf6c4494649
kokotata421 <kokotata421@gmail.com>
2019-08-27 03:12:42 +0900

    [add 🎉] homeViewModelを追加

---
+++ iOSArchitectureKKSample/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/27.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+final class HomeViewModel {
+    
+}
