b6d9594f186f7d3f71440713d43ea0937cec3bbf
kokotata421 <kokotata421@gmail.com>
2019-08-26 18:16:32 +0900

    [add ✍️]スプラッシュViewModelクラスを追加

---
+++ iOSArchitectureKKSample/SplashViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SplashViewModel.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/26.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+class SplashViewModel {
+    
+}
