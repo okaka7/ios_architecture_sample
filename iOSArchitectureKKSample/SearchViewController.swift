1c91a9e17bce9e08cbe232a18ba9328e40531ce7
kokotata421 <kokotata421@gmail.com>
2019-08-27 21:10:51 +0900

    [change 🎉]search画面の真ん中のラベルをボタンに変える。(SearchControllerのisActiveプロパティによる挙動の確認のため)

--- iOSArchitectureKKSample/SearchViewController.swift
+++ iOSArchitectureKKSample/SearchViewController.swift
@@ -7,26 +7,37 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class SearchViewController: UIViewController {
     
     lazy private var searchController: UISearchController = {
-        
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         self.navigationItem.searchController = searchController
-
         return searchController
     }()
     
-    lazy private var label: UILabel = {
-        let label: UILabel = .init(frame: .zero)
-        label.text = "search"
-        label.sizeToFit()
-        label.center = self.view.center
-        return label
+//    lazy private var label: UILabel = {
+//        let label: UILabel = .init(frame: .zero)
+//        label.text = "search"
+//        label.sizeToFit()
+//        label.center = self.view.center
+//        return label
+//    }()
+    
+    lazy private var button: UIButton = {
+        let button: UIButton = .init(frame: .zero)
+        button.setTitle("button", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.sizeToFit()
+        button.center = self.view.center
+        return button
     }()
+    
+    private let disposeBag = DisposeBag()
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -43,11 +54,19 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(label)
+      //  self.view.addSubview(label)
+        self.view.addSubview(button)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         _ = self.searchController
+        
+        button.rx.tap.subscribe(onNext: { [weak self] in
+            guard let self = self else {
+                return
+            }
+            self.searchController.isActive = !self.searchController.isActive
+        }).disposed(by: disposeBag)
     }
 
     /*
