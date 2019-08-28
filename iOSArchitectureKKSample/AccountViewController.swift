81d370eb8f2669235050500d6b199b53bdde8931
kokotata421 <kokotata421@gmail.com>
2019-08-28 20:16:47 +0900

    [change 🎉] 不必要なコメントの削除

--- iOSArchitectureKKSample/AccountViewController.swift
+++ iOSArchitectureKKSample/AccountViewController.swift
@@ -35,15 +35,5 @@ class AccountViewController: UIViewController {
 
         self.view.addSubview(label)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
