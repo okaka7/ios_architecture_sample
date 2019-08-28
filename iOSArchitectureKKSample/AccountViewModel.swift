00e1c158ad91ae6e5497042ddde7b74a1e13d91a
kokotata421 <kokotata421@gmail.com>
2019-08-28 23:19:56 +0900

    [add 🎉] AccountViewModelプロトコルとクラスを追加

---
+++ iOSArchitectureKKSample/AccountViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  AccountViewModel.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/28.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol AccountViewModelProtocol {
+    
+}
+
+final class AccountViewModel: AccountViewModelProtocol {
+    
+}
