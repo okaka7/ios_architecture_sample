549c4d9fe4cbe8ce1520fdca643f3d104ec06da5
kokotata421 <kokotata421@gmail.com>
2019-08-28 23:18:53 +0900

    [add 🎉]SearchViewModelプロトコルとクラスを作成

---
+++ iOSArchitectureKKSample/SearchViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewModel.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/28.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchViewModelProtocol {
+    
+}
+
+final class SearchViewModel: SearchViewModelProtocol {
+    
+}
