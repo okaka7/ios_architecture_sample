9f556ba540c186547f5d052257fcc8e9f1c1b5f9
kokotata421 <kokotata421@gmail.com>
2019-08-28 20:15:38 +0900

    [add 🎉] AccountViewのインターフェースアダプターであるAccountViewInputPortプロトコル、AccountController, AccountViewPresenterクラスを追加

---
+++ iOSArchitectureKKSample/AccountViewAdapter.swift
@@ -0,0 +1,21 @@
+//
+//  AccountViewAdapter.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/08/28.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+protocol AccountViewInputPort: class {
+   
+}
+
+final class AccountController: AccountViewInputPort {
+   
+}
+
+final class AccountViewPresenter {
+    
+}
