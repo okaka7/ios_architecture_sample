f58233ee735ad24a840e730283a361496b345963
kokotata421 <kokotata421@gmail.com>
2019-09-10 23:29:23 +0900

    [add 🎉]ログイン状態を管理するLoginEnumを追加

---
+++ iOSArchitectureKKSample/LogIn.swift
@@ -0,0 +1,15 @@
+//
+//  LogIn.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/09/10.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+enum LogIn: Int {
+    case noAccessKey
+    case logIn
+    case logOut
+}
