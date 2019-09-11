6c5b6c16a1c2134b78c42c803a6259c1adb0ca0c
kokotata421 <kokotata421@gmail.com>
2019-09-12 01:08:04 +0900

    [add 🎉] HomeViewAdapterのfetchTopPhotosメソッドを実装

--- iOSArchitectureKKSample/HomeViewAdapter.swift
+++ iOSArchitectureKKSample/HomeViewAdapter.swift
@@ -6,8 +6,10 @@
 //  Copyright © 2019 Kota Kawanishi. All rights reserved.
 //
 import Foundation
+import RxSwift
+import RxCocoa
 
-protocol HomeViewInputPort {
+protocol HomeViewInputPort: class {
     func fetchTopPhotos(page: Int)
     func selectPhoto(_ photo: PhotoUIEntity)
     func switchPhoto(_ photo: PhotoUIEntity)
@@ -17,11 +19,19 @@ extension HomeViewInputPort {
     func fetchTopPhotos(page: Int = 1) { }
 }
 
-struct HomeViewAdapter: HomeViewInputPort {
+final class HomeViewAdapter: HomeViewInputPort {
     let useCase: HomeUseCaseInputPort
+    private let disposeBag: DisposeBag
     
-    init(useCase: HomeUseCaseInputPort) {
+    lazy private(set) var topPhotosObservable: Observable<TopPhotoList?> = {
+        return self.topPhotosSubject.asObservable()
+    }()
+    private let topPhotosSubject: PublishRelay<TopPhotoList?> = .init()
+    
+    init(useCase: HomeUseCaseInputPort,
+         disposeBag: DisposeBag = DisposeBag()) {
         self.useCase = useCase
+        self.disposeBag = disposeBag
     }
     
     func selectPhoto(_ photo: PhotoUIEntity) {
@@ -32,7 +42,27 @@ struct HomeViewAdapter: HomeViewInputPort {
         
     }
     
-    func fetchTopPhotos() {
-        
+    func fetchTopPhotos(page: Int = 1) {
+        var topPhotos: UnsplashPhotosTarget.Response = .init()
+        useCase.fetchTopPhotos(page: page)
+            .subscribe(onSuccess: { [weak self] photos in
+                guard let self = self else {
+                    return
+                }
+                let appendedPhotos: UnsplashPhotosTarget.Response = photos.filter { $0.isSuitableForTopImage }
+                topPhotos.append(contentsOf: appendedPhotos)
+                if topPhotos.count >= 20 {
+                    self.topPhotosSubject.accept(TopPhotoList(photos: topPhotos))
+                } else {
+                    self.fetchTopPhotos(page: page + 1)
+                }
+                },
+                       onError: { error in
+                        #if DEBUG
+                        log.debug(error)
+                        #endif
+                        self.topPhotosSubject.accept(nil)
+            })
+            .disposed(by: disposeBag)
     }
 }
