5bf48c6fa773fec396ab9b51dab5430854f9de88
kokotata421 <kokotata421@gmail.com>
2019-09-12 01:34:14 +0900

    [delete 🎉] 必要なくなったメソッド変数をSplashViewAdapterクラスから削除

--- iOSArchitectureKKSample/SplashViewAdapter.swift
+++ iOSArchitectureKKSample/SplashViewAdapter.swift
@@ -11,20 +11,18 @@ import RxSwift
 import RxCocoa
 
 protocol SplashControllerProtocol: class {
-    func fetchPopularPhotos(page: Int)
+    //func fetchPopularPhotos(page: Int)
     //func fetchAccount()
 }
 
 extension SplashControllerProtocol {
-    func fetchPopularPhotos(page: Int = 1) {
-        
-    }
+  
 }
 
 protocol SplashPresenterProtocol: class {
-    var popularPhotosObservable: Observable<PopularPhotoList?> { get }
-    var topPhotosObservable: Observable<TopPhotoList?> { get }
-    //var accountObservable: Observable<UnsplashAccountTarget.Response?> { get }
+//    var popularPhotosObservable: Observable<PopularPhotoList?> { get }
+//    var topPhotosObservable: Observable<TopPhotoList?> { get }
+//    //var accountObservable: Observable<UnsplashAccountTarget.Response?> { get }
     
 }
 
@@ -32,73 +30,11 @@ final class SplashViewAdapter: SplashControllerProtocol, SplashPresenterProtocol
     private let useCase: PrepareAppUseCaseInputPort
     private let disposeBag: DisposeBag
     
-    lazy private(set) var popularPhotosObservable: Observable<PopularPhotoList?> = {
-        return self.popularPhotosSubject.asObservable()
-    }()
-    private let popularPhotosSubject: PublishRelay<PopularPhotoList?> = .init()
-    
-    lazy private(set) var topPhotosObservable: Observable<TopPhotoList?> = {
-        return self.topPhotosSubject.asObservable()
-    }()
-    private let topPhotosSubject: PublishRelay<TopPhotoList?> = .init()
-
-//    private let accountSubject: PublishRelay<UnsplashAccountTarget.Response?> = .init()
-//
-//    lazy private(set) var accountObservable: Observable<UnsplashAccountTarget.Response?> = {
-//        return self.accountSubject.asObservable()
-//    }()
-    
     init (useCase: PrepareAppUseCaseInputPort,
           disposeBag: DisposeBag = DisposeBag()) {
         self.useCase = useCase
         self.disposeBag = disposeBag
     }
     
-    func fetchPopularPhotos(page: Int = 1) {
-        var popularPhotos: UnsplashPhotosTarget.Response = .init()
-        var topPhotos: UnsplashPhotosTarget.Response = .init()
-        useCase.fetchPopularPhotos(page: 1)
-            .subscribe(onSuccess: { [weak self] photos in
-                guard let self = self else {
-                    return
-                }
-                popularPhotos.append(contentsOf: photos)
-                let appendedTopPhotos: UnsplashPhotosTarget.Response = photos.filter { $0.isSuitableForTopImage }
-                topPhotos.append(contentsOf: appendedTopPhotos)
-                if topPhotos.count >= 20 {
-                    self.topPhotosSubject.accept(TopPhotoList(photos: topPhotos))
-                    self.popularPhotosSubject.accept(PopularPhotoList(photos: popularPhotos))
-                } else {
-                    self.fetchPopularPhotos(page: page + 1)
-                }
-            },
-                       onError: { error in
-                        #if DEBUG
-                        log.debug(error)
-                        #endif
-                        self.topPhotosSubject.accept(nil)
-                        self.popularPhotosSubject.accept(nil)
-            })
-            .disposed(by: disposeBag)
-    }
-    
-//    func fetchAccount() {
-//        useCase.fetchAccount()
-//            .subscribe(onSuccess: { [weak self] account in
-//                    guard let self = self else {
-//                        return
-//                    }
-//                    self.accountSubject.accept(account)
-//                },
-//                       onError: { [weak self] error in
-//                        guard let self = self else {
-//                            return
-//                        }
-//                        #if DEBUG
-//                        log.debug(error)
-//                        #endif
-//                        self.accountSubject.accept(nil)
-//            })
-//            .disposed(by: disposeBag)
-//    }
+
 }
