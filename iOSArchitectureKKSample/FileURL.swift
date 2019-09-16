2f98da05a7dc8a7fba7824a8c69d8acbc74911a8
kokotata421 <kokotata421@gmail.com>
2019-09-17 03:59:01 +0900

    [add 🎉] 画像URLから画像を取得してローカルに保存、またローカルから保存した画像を取得するFile構造体作成

---
+++ iOSArchitectureKKSample/FileURL.swift
@@ -0,0 +1,42 @@
+//
+//  FileURL.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/09/17.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+struct File {
+    // DocumentディレクトリのfileURLを取得
+    static func getDocumentsURL() -> NSURL {
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
+        return documentsURL
+    }
+    
+    // ディレクトリのパスにファイル名をつなげてファイルのフルパスを作る
+    static func fileInDocumentsDirectory(filename: String) -> String {
+        let fileURL = getDocumentsURL().appendingPathComponent(filename)
+        return fileURL!.path
+    }
+    
+    static func saveImage (image: UIImage, path: String ) -> Bool {
+        let pngImageData = image.pngData()
+        do {
+            try pngImageData!.write(to: URL(fileURLWithPath: path), options: .atomic)
+        } catch {
+            print(error)
+            return false
+        }
+        return true
+    }
+    
+    static func loadImageFromPath(path: String) -> UIImage? {
+        let image = UIImage(contentsOfFile: path)
+        if image == nil {
+            print("missing image at: \(path)")
+        }
+        return image
+    }
+}
