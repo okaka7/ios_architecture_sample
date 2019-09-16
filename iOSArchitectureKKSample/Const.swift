3963f63b1c46189f8d0efaa756b1adde07c95977
kokotata421 <kokotata421@gmail.com>
2019-09-17 01:33:48 +0900

    [add 🎉] Cons.swifttファイルと構造体を追加

---
+++ iOSArchitectureKKSample/Const.swift
@@ -0,0 +1,11 @@
+//
+//  Const.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/09/17.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import Foundation
+
+struct Const {}
