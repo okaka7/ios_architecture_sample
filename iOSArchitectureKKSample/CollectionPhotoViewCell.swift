d596dcb78b82b3d12862edce8e903cdf452ea16b
kokotata421 <kokotata421@gmail.com>
2019-09-17 20:57:00 +0900

    [add 🎉]CollectionViewで写真を表示するCellクラスを追加

---
+++ iOSArchitectureKKSample/CollectionPhotoViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CollectionPhotoViewCell.swift
+//  iOSArchitectureKKSample
+//
+//  Created by Kota Kawanishi on 2019/09/17.
+//  Copyright © 2019 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPhotoViewCell: UICollectionViewCell {
+    var image: UIImage?
+    
+    func configureImage(image: UIImage) {
+        self.image = image
+        
+        let imageView = UIImageView(image: image)
+        self.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        imageView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+    }
+}
