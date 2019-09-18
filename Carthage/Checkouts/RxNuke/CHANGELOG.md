adf81fd0369a8294eafbefea7bc3394236db7970
kokotata421 <kokotata421@gmail.com>
2019-09-18 09:25:44 +0900

    [add 🎉] 画像取得キャッシュのためにRxNukeライブラリを導入

---
+++ Carthage/Checkouts/RxNuke/CHANGELOG.md
@@ -0,0 +1,36 @@
+ [Changelog](https://github.com/kean/RxNuke/releases) for all versions
+
+## RxNuke 0.8.1
+
+- Fix Carthage support by adding a shared scheme 
+
+## RxNuke 0.8
+
+- Add Swift 5.0 support – [Takuma Osada](https://github.com/takumaosada) in [#11](https://github.com/kean/RxNuke/pull/11)
+- Remove Swift 4.0 and Swift 4.1 support
+- Remove iOS 9, tvOS 9, watchOS 2.0, macOS 10.10 and macOS 10.11 support
+
+## RxNuke 0.7
+
+- #7, Move methods to `Reactive` extension which is a standard practice in RxSwift. Thanks to @jdisho.
+
+## RxNuke 0.6
+
+- Add missing `public func loadImage(with url: URL) -> RxSwift.Single<Nuke.ImageResponse>` function
+- Remove deprecated code
+
+## RxNuke 0.5
+
+- Updated for Nuke 7
+
+## RxNuke 0.4
+
+- Updated for Nuke 6
+
+## RxNuke 0.2
+
+- Migrate to Swift 4
+
+## RxNuke 0.1
+
+- Initial version
---
+++ Carthage/Checkouts/RxNuke/Source/RxNuke.swift
@@ -0,0 +1,47 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017-2019 Alexander Grebenyuk (github.com/kean).
+
+import Nuke
+import RxSwift
+
+#if !os(macOS)
+import UIKit
+#else
+import AppKit
+#endif
+
+extension ImagePipeline: ReactiveCompatible {}
+
+public extension Reactive where Base: ImagePipeline {
+    /// Loads an image with a given url. Emits the value synchronously if the
+    /// image was found in memory cache.
+  func loadImage(with url: URL) -> Single<ImageResponse> {
+        return self.loadImage(with: ImageRequest(url: url))
+    }
+
+    /// Loads an image with a given request. Emits the value synchronously if the
+    /// image was found in memory cache.
+  func loadImage(with request: ImageRequest) -> Single<ImageResponse> {
+        return Single<ImageResponse>.create { single in
+            if let image = self.cachedResponse(for: request) {
+                single(.success(image)) // return synchronously
+                return Disposables.create() // nop
+            } else {
+                let task = self.base.loadImage(with: request) { response, error in
+                    if let response = response {
+                        single(.success(response))
+                    } else {
+                        single(.error(error ?? ImagePipeline.Error.processingFailed)) // error always non-nil
+                    }
+                }
+                return Disposables.create { task.cancel() }
+            }
+        }
+    }
+
+    private func cachedResponse(for request: ImageRequest) -> ImageResponse? {
+        guard request.memoryCacheOptions.isReadAllowed else { return nil }
+        return base.configuration.imageCache?.cachedResponse(for: request)
+    }
+}
